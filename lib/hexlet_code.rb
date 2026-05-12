217714d0dd131cc3037d8a486a0a3c3a33fb3c10
a.sarkisyants <a.sarkisyants@sftpro.ru>
2026-05-13 01:29:40 +0900

    second-task

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
+require "zeitwerk"
+
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
 end
